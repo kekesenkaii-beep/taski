192c2bcb706b4fd37151d853d8a70ef5585995fa
Ubuntu <ubuntu@fv42qspmlbvoclflpoca.auto.internal>
2026-06-14 06:06:34 +0000

    Del python_dotenv

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,16 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    'django-insecure-default-key-for-tests'
+)
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+DEBUG = False
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['81.26.179.20', '127.0.0.1', 'localhost', 'kekesenkaii-beep.ddns.net']
 
 # Application definition
 
@@ -103,7 +105,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
